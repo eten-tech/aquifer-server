bb98b86dc349fe0abdf24410f7139e4982365ae1
kaseywright <kasey.wright@biblionexus.org>
2026-04-28 13:31:54 -0400

    Format

--- src/Aquifer.Data/Migrations/20260428000000_FixDivideByZeroInPublishedItemsAndWordsByLanguageReport.cs
+++ src/Aquifer.Data/Migrations/20260428000000_FixDivideByZeroInPublishedItemsAndWordsByLanguageReport.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
@@ -10,31 +11,33 @@ namespace Aquifer.Data.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql("""
-                UPDATE Reports
-                SET SqlStatement = ';WITH EnglishCounts AS (SELECT PR.Id,COUNT(DISTINCT RC.Id) AS ItemCount,ISNULL(SUM(RCV.WordCount),0) AS WordCount FROM ResourceContentVersions RCV INNER JOIN ResourceContents RC ON RC.Id=RCV.ResourceContentId INNER JOIN Resources R ON R.Id=RC.ResourceId INNER JOIN ParentResources PR ON PR.Id=R.ParentResourceId INNER JOIN Languages LAN ON LAN.Id=RC.LanguageId WHERE RCV.IsPublished=1 AND LAN.Id=1 AND(@ParentResourceId=0 OR @ParentResourceId=PR.Id) GROUP BY PR.Id)
-SELECT PR.DisplayName AS Resource,LAN.EnglishDisplay AS Language,COUNT(DISTINCT RC.Id) AS ItemCount,SUM(RCV.WordCount) AS WordCount,FORMAT(SUM(RCV.SourceWordCount)/CAST(NULLIF(EC.WordCount, 0) AS decimal),''P'') AS [% Complete]
-FROM ResourceContentVersions RCV INNER JOIN ResourceContents RC ON RC.Id=RCV.ResourceContentId INNER JOIN Resources R ON R.Id=RC.ResourceId INNER JOIN ParentResources PR ON PR.Id=R.ParentResourceId INNER JOIN Languages LAN ON LAN.Id=RC.LanguageId INNER JOIN EnglishCounts EC ON EC.Id=PR.Id
-WHERE RCV.IsPublished=1 AND(@LanguageId=0 OR @LanguageId=LAN.Id) AND(@ParentResourceId=0 OR @ParentResourceId=PR.Id)
-GROUP BY PR.DisplayName,LAN.EnglishDisplay,EC.WordCount
-ORDER BY PR.DisplayName,LAN.EnglishDisplay'
-                WHERE Slug = 'published-items-and-words-by-language'
-                """);
+            migrationBuilder.Sql(new StringBuilder().Append("""
+                                                            UPDATE Reports
+                                                            SET SqlStatement = ';WITH EnglishCounts AS (SELECT PR.Id,COUNT(DISTINCT RC.Id) AS ItemCount,ISNULL(SUM(RCV.WordCount),0) AS WordCount FROM ResourceContentVersions RCV INNER JOIN ResourceContents RC ON RC.Id=RCV.ResourceContentId INNER JOIN Resources R ON R.Id=RC.ResourceId INNER JOIN ParentResources PR ON PR.Id=R.ParentResourceId INNER JOIN Languages LAN ON LAN.Id=RC.LanguageId WHERE RCV.IsPublished=1 AND LAN.Id=1 AND(@ParentResourceId=0 OR @ParentResourceId=PR.Id) GROUP BY PR.Id)
+                                                            SELECT PR.DisplayName AS Resource,LAN.EnglishDisplay AS Language,COUNT(DISTINCT RC.Id) AS ItemCount,SUM(RCV.WordCount) AS WordCount,FORMAT(SUM(RCV.SourceWordCount)/CAST(NULLIF(EC.WordCount, 0) AS decimal),''P'') AS [% Complete]
+                                                            FROM ResourceContentVersions RCV INNER JOIN ResourceContents RC ON RC.Id=RCV.ResourceContentId INNER JOIN Resources R ON R.Id=RC.ResourceId INNER JOIN ParentResources PR ON PR.Id=R.ParentResourceId INNER JOIN Languages LAN ON LAN.Id=RC.LanguageId INNER JOIN EnglishCounts EC ON EC.Id=PR.Id
+                                                            WHERE RCV.IsPublished=1 AND(@LanguageId=0 OR @LanguageId=LAN.Id) AND(@ParentResourceId=0 OR @ParentResourceId=PR.Id)
+                                                            GROUP BY PR.DisplayName,LAN.EnglishDisplay,EC.WordCount
+                                                            ORDER BY PR.DisplayName,LAN.EnglishDisplay'
+                                                            WHERE Slug = 'published-items-and-words-by-language'
+                                                            """)
+                .ToString());
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql("""
-                UPDATE Reports
-                SET SqlStatement = ';WITH EnglishCounts AS (SELECT PR.Id,COUNT(DISTINCT RC.Id) AS ItemCount,ISNULL(SUM(RCV.WordCount),0) AS WordCount FROM ResourceContentVersions RCV INNER JOIN ResourceContents RC ON RC.Id=RCV.ResourceContentId INNER JOIN Resources R ON R.Id=RC.ResourceId INNER JOIN ParentResources PR ON PR.Id=R.ParentResourceId INNER JOIN Languages LAN ON LAN.Id=RC.LanguageId WHERE RCV.IsPublished=1 AND LAN.Id=1 AND(@ParentResourceId=0 OR @ParentResourceId=PR.Id) GROUP BY PR.Id)
-SELECT PR.DisplayName AS Resource,LAN.EnglishDisplay AS Language,COUNT(DISTINCT RC.Id) AS ItemCount,SUM(RCV.WordCount) AS WordCount,FORMAT(SUM(RCV.SourceWordCount)/CAST(EC.WordCount AS decimal),''P'') AS [% Complete]
-FROM ResourceContentVersions RCV INNER JOIN ResourceContents RC ON RC.Id=RCV.ResourceContentId INNER JOIN Resources R ON R.Id=RC.ResourceId INNER JOIN ParentResources PR ON PR.Id=R.ParentResourceId INNER JOIN Languages LAN ON LAN.Id=RC.LanguageId INNER JOIN EnglishCounts EC ON EC.Id=PR.Id
-WHERE RCV.IsPublished=1 AND(@LanguageId=0 OR @LanguageId=LAN.Id) AND(@ParentResourceId=0 OR @ParentResourceId=PR.Id)
-GROUP BY PR.DisplayName,LAN.EnglishDisplay,EC.WordCount
-ORDER BY PR.DisplayName,LAN.EnglishDisplay'
-                WHERE Slug = 'published-items-and-words-by-language'
-                """);
+            migrationBuilder.Sql(new StringBuilder().Append("""
+                                                            UPDATE Reports
+                                                            SET SqlStatement = ';WITH EnglishCounts AS (SELECT PR.Id,COUNT(DISTINCT RC.Id) AS ItemCount,ISNULL(SUM(RCV.WordCount),0) AS WordCount FROM ResourceContentVersions RCV INNER JOIN ResourceContents RC ON RC.Id=RCV.ResourceContentId INNER JOIN Resources R ON R.Id=RC.ResourceId INNER JOIN ParentResources PR ON PR.Id=R.ParentResourceId INNER JOIN Languages LAN ON LAN.Id=RC.LanguageId WHERE RCV.IsPublished=1 AND LAN.Id=1 AND(@ParentResourceId=0 OR @ParentResourceId=PR.Id) GROUP BY PR.Id)
+                                                            SELECT PR.DisplayName AS Resource,LAN.EnglishDisplay AS Language,COUNT(DISTINCT RC.Id) AS ItemCount,SUM(RCV.WordCount) AS WordCount,FORMAT(SUM(RCV.SourceWordCount)/CAST(EC.WordCount AS decimal),''P'') AS [% Complete]
+                                                            FROM ResourceContentVersions RCV INNER JOIN ResourceContents RC ON RC.Id=RCV.ResourceContentId INNER JOIN Resources R ON R.Id=RC.ResourceId INNER JOIN ParentResources PR ON PR.Id=R.ParentResourceId INNER JOIN Languages LAN ON LAN.Id=RC.LanguageId INNER JOIN EnglishCounts EC ON EC.Id=PR.Id
+                                                            WHERE RCV.IsPublished=1 AND(@LanguageId=0 OR @LanguageId=LAN.Id) AND(@ParentResourceId=0 OR @ParentResourceId=PR.Id)
+                                                            GROUP BY PR.DisplayName,LAN.EnglishDisplay,EC.WordCount
+                                                            ORDER BY PR.DisplayName,LAN.EnglishDisplay'
+                                                            WHERE Slug = 'published-items-and-words-by-language'
+                                                            """)
+                .ToString());
         }
     }
 }
